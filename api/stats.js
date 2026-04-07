cb8f880643036ac5dd4baa9762d9f396769d7759
CXXN <rifinaxd@gmail.com>
2026-04-07 16:50:23 -0300

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,11 +1,20 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { id } = req.query;
 
-  global.links = global.links || {};
+  const SUPABASE_URL = "https://bxtzdxehpsyvziiqwpza.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_klwriPR0NcsJtlIqIj3TJA_7hebCQFd";
 
-  if (!global.links[id]) {
-    return res.status(200).json({ locations: [] });
-  }
+  const response = await fetch(
+    `${SUPABASE_URL}/rest/v1/locations?id=eq.${id}&order=created_at.desc`,
+    {
+      headers: {
+        'apikey': SUPABASE_KEY,
+        'Authorization': `Bearer ${SUPABASE_KEY}`
+      }
+    }
+  );
 
-  res.status(200).json(global.links[id]);
+  const data = await response.json();
+
+  res.status(200).json({ locations: data });
 }
